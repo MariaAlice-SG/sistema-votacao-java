2e15442d7e55795008bf4d548ef9a3ce91e16076
Maria Alice Sousa Gonçalves <maria.7903966@aluno.mg.gov.br>
2026-08-24 07:31:32 -0300

    feat: definir limites de candidatos turmas e votantes

--- SistemaVotacao.java
+++ SistemaVotacao.java
@@ -1,18 +1,17 @@
+
 import java.util.Scanner;
 
 public class SistemaVotacao {
+
+    static final int MAX_CANDIDATOS = 5;
+    static final int TOTAL_TURMAS = 3;
+    static final int MAX_VOTANTES_POR_TURMA = 10;
 
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Sistema de votação iniciado.");
+
         scanner.close();
     }
 }
-static final int MAX_CANDIDATOS = 5;
-static final int TOTAL_TURMAS = 3;
-static final int MAX_VOTANTES_POR_TURMA = 10;
-
-void main() {
-}
-
