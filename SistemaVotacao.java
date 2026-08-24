f8cf72c16217d1fc78b574c0db3826cf37f77aa9
Maria Alice Sousa Gonçalves <maria.7903966@aluno.mg.gov.br>
2026-08-24 07:38:09 -0300

    feat: implementar menu principal com do-while

--- SistemaVotacao.java
+++ SistemaVotacao.java
@@ -12,20 +12,57 @@ public class SistemaVotacao {
     static String[] nomesCandidatos = new String[MAX_CANDIDATOS];
     static int[] votosCandidatos = new int[MAX_CANDIDATOS];
 
-    // Matriz de votos por turma
     static int[][] votosPorTurma =
             new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
 
-    // Quantidade de votos de cada turma
     static int[] quantidadeVotosTurma =
             new int[TOTAL_TURMAS];
 
-    // Quantidade de candidatos cadastrados
     static int quantidadeCandidatos = 0;
 
 
     public static void main(String[] args) {
         System.out.println("Sistema de votação iniciado.");
+        int opcao;
+
+        do {
+            System.out.println("\n===== SISTEMA DE VOTAÇÃO =====");
+            System.out.println("1 - Cadastrar candidatos");
+            System.out.println("2 - Iniciar votação");
+            System.out.println("3 - Exibir resultado");
+            System.out.println("4 - Exibir matriz de votos");
+            System.out.println("5 - Sair");
+
+            System.out.print("Opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("Cadastro selecionado.");
+                    break;
+
+                case 2:
+                    System.out.println("Votação selecionada.");
+                    break;
+
+                case 3:
+                    System.out.println("Resultado selecionado.");
+                    break;
+
+                case 4:
+                    System.out.println("Matriz selecionada.");
+                    break;
+
+                case 5:
+                    System.out.println("Sistema encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+
+        } while (opcao != 5);
 
         scanner.close();
     }
